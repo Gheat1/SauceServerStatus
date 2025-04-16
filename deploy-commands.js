058d6dfe99422e8a0a91437a247d5139b6db151d
Gheat <64615952+Gheat1@users.noreply.github.com>
2025-04-16 09:12:45 -0700

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,24 +2,20 @@ const { SlashCommandBuilder, REST, Routes } = require('discord.js');
 require('dotenv').config();
 
 const commands = [
-  new SlashCommandBuilder()
-    .setName('status')
-    .setDescription('Check Shredsauce server status'),
-  new SlashCommandBuilder()
-    .setName('setup')
-    .setDescription('Choose a channel to receive alerts'),
+  new SlashCommandBuilder().setName('status').setDescription('Check if the Sauce server is up'),
+  new SlashCommandBuilder().setName('setchannel').setDescription('Set the channel to send Sauce server status alerts')
 ].map(cmd => cmd.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    console.log('⏳ Deploying commands...');
+    console.log('Deploying slash commands...');
     await rest.put(
       Routes.applicationCommands(process.env.CLIENT_ID),
-      { body: commands },
+      { body: commands }
     );
-    console.log('✅ Commands deployed');
+    console.log('✅ Slash commands deployed.');
   } catch (err) {
     console.error(err);
   }
